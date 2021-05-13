2e37ddeeda2178ad366d30474ff19b39738bad2f
carlos <carlos.h.goncalves@outlook.com>
2021-05-14 00:08:34 +0100

    CheckOut form created
    CheckOut form was created,

--- movie_renting/src/model/Costumer.java
+++ movie_renting/src/model/Costumer.java
@@ -16,7 +16,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Customer represents a non-subscribed person that uses the machine
  * @thiag 
  */
 public class Customer implements Model<Customer>{
