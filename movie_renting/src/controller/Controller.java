d7cf63bedb0b1fc964a47c56463c80927ba61fa5
thiagocruzfurtado <thiagojcf@live.com>
2021-05-15 22:32:15 +0100

    Controllers full version: thiagojcf@live.com
    Author: thiagocruzfurtado<thiagojcf@live.com>

--- movie_renting/src/controller/Controller.java
+++ movie_renting/src/controller/Controller.java
@@ -5,13 +5,17 @@
  */
 package controller;
 
+import exception.InvalidNumberMoviesException;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import model.Customer;
 import model.Movie;
+import model.Rental;
 
 /**
  *
@@ -19,21 +23,32 @@ import model.Movie;
  */
 public class Controller {
     
-    private List<Movie> movies; 
     
-    public Controller() throws SQLException{
-        this.movies = null; 
+    private Customer customer; 
+    
+    public Controller(){
+        this.customer = new Customer();
     }
-    
     /**
      * @return 
     */
-    public List<Movie> listMovies(){
-        if(this.movies == null){
-            Movie movie = new Movie();
-            this.movies = movie.list("available", "1");
+    public List<String[]> listMovies(){
+       
+        List<String[]> available = this.listAllAvailableMovies();
+        List<String[]> moviesCustomer = this.listCustomerMovies();
+        
+        Iterator<String[]> i = available.iterator();
+
+        while(i.hasNext()) {
+            String [] movieS = i.next();
+            for(String [] cMovie : moviesCustomer){
+                if(movieS[0].equalsIgnoreCase(cMovie[0])){
+                    i.remove();
+                }
+            }
         }
-        return this.movies;
+        
+        return available;
     }
     
     /**
@@ -80,65 +95,69 @@ public class Controller {
         return moviesString;
     }
     
-    public boolean checkoutMovie(Movie movie){
-        return false;
+    public void checkOut(String cardNumber, String cvv, String email, String offerCode) throws SQLException{
+        this.customer = getCustomer(cardNumber, cvv, email);
+        this.customer.checkOut(offerCode);
     }
     
-  
-    
-    public static void main(String [] args){
-        
-        Movie movie = new Movie(01, "Harry Potter and Philosopher Stone", "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.", "");
-        //movie.save();
-        movie.setAvailable(false);
-        movie.update();
-        movie.setAvailable(true);
-        movie.update();
-        
-        Movie m2 = movie.get("movie_id", "1");
-        System.out.println(m2);
-        
-        List<Movie> movies = movie.list("available", "1");
-        System.out.printf("Size list %d", movies.size());
-        
-        Customer customer = new Customer("1234567891011", "123");
-        System.out.println(customer);
-        customer.setCVV("321");
-        customer.update();
-        Customer customer2 = customer.get("card_number", "1234567891011");
-        System.out.println(customer2);
-        List<Customer> customers = customer.list("1", "1");
-        System.out.printf("Size list %d", customers.size());
-        //customer.save();
-        
-    }
     
     private Customer getCustomer(String cardNumber, String cvv, String email){
         Customer customer = new Customer();
         customer = customer.get("card_number", cardNumber);
-        if(customer == null)
-           customer = new Customer(cardNumber, cvv, email, 1);
+        if(customer == null){
+           this.customer.setCardNumber(cardNumber);
+           this.customer.setCVV(cvv);
+           this.customer.setEmail(email);
+           customer = this.customer;
+        }
         return customer;
     }
     
-    private LocalDateTime getExpectedRetunDate(LocalDateTime rentalDate, Customer customer, int moviesAmount){
+    private LocalDateTime getExpectedRetunDate(LocalDateTime rentalDate, Customer customer, int moviesAmount) throws InvalidNumberMoviesException{
         
         LocalDateTime expectedRetunDate = null;
         
         if(customer.isFirstRental()){
-            LocalDateTime expectedReturnDate = rentalDate.plusDays(1);   
+            expectedRetunDate = rentalDate.plusDays(1);   
         } else if(moviesAmount == 1){
-            LocalDateTime expectedReturnDate = rentalDate.plusDays(1);   
+            expectedRetunDate = rentalDate.plusDays(1);   
         } else if(moviesAmount == 2){
-            LocalDateTime expectedReturnDate = rentalDate.plusDays(2);   
+            expectedRetunDate = rentalDate.plusDays(2);   
         } else if(moviesAmount == 3){
-            LocalDateTime expectedReturnDate = rentalDate.plusDays(3);   
+            expectedRetunDate = rentalDate.plusDays(3);   
         } else if(moviesAmount == 4) {
-            LocalDateTime expectedReturnDate = rentalDate.plusDays(4);   
+            expectedRetunDate = rentalDate.plusDays(4);   
         } else{
-            //throw
+            String message = String.format("Invalid Number of Movies in the Shopping Basket: %d."
+                    + "If this is your first time renting, you can only rent the maximum number of 2 movies, otherwise the maximum is 4.", moviesAmount);
+            throw new InvalidNumberMoviesException(message);
         }
         
         return LocalDateTime.of(expectedRetunDate.toLocalDate(), LocalTime.of(8, 0, 0));
+    }
+    
+    private boolean checkFirstRentalRules(Customer customer, int moviesAmount){
+         if(!customer.isFirstRental()){
+             return moviesAmount > 0 && moviesAmount <= 4;
+        } else {
+             return moviesAmount > 0 && moviesAmount <= 2;
+        }
+    }
+    
+    private List<String[]> listAllAvailableMovies(){
+        Movie movie = new Movie();
+        List<Movie> movies = movie.list("available", "1");
+        List<String[]> moviesString = new ArrayList<>();
+        for(Movie m: movies){
+            String [] mString = {String.valueOf(m.getId()), m.getTitle(), m.getDescription()};
+            moviesString.add(mString);
+        }
+        return moviesString;
+    }
+
+    public String[] returnMovie(int id) throws SQLException {
+        Rental rental = new Rental();
+        rental = rental.get("movie_id", String.valueOf(id));
+        return rental.finish();
     }
 }
